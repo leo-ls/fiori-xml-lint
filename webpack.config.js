b2397edab4a3e8db41584f62e931cf5ef2c88479
Leonardo Schmidt <schmil15@nala.roche.com>
2020-05-16 14:48:36 -0300

    Fixing bundling on webpack, moving to yarn

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,65 @@
 //@ts-check
 'use strict';
 
-const path = require('path');
+const
+    webpack = require('webpack'),
+    path = require('path'),
+    CopyPlugin = require('copy-webpack-plugin');
 
-/**@type {import('webpack').Configuration}*/
-const config = {
-    target: 'node',
-    entry: './src/extension.ts',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'extension.js',
-        libraryTarget: "commonjs2",
-        devtoolModuleFilenameTemplate: "../[resource-path]",
-    },
-    devtool: 'source-map',
-    externals: [
-        {
-            vscode: "commonjs vscode"
-        },
-        /^@sap/
-    ],
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    module: {
-        rules: [{
-            test: /\.ts$/,
-            exclude: /node_modules/,
-            use: [{
-                loader: 'ts-loader',
-                options: {
-                    compilerOptions: {
-                        "module": "es6"
+/**@type {webpack.ConfigurationFactory}*/
+const config = function (env) {
+
+    const plugins = [
+        env === 'production' && new CopyPlugin({
+            patterns: [
+                {
+                    from: 'node_modules/@sap/**',
+                    globOptions: {
+                        ignore: [
+                            '**/@sap/**/node_modules/**/*',
+                            '**/@sap/**/test/**/*'
+                        ]
                     }
                 }
+            ]
+        })
+    ].filter(Boolean);
+
+    return {
+        target: 'node',
+        entry: './src/extension.ts',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'extension.js',
+            libraryTarget: "commonjs2",
+            devtoolModuleFilenameTemplate: "../[resource-path]",
+        },
+        devtool: 'source-map',
+        externals: [
+            {
+                vscode: "commonjs vscode"
+            },
+            /^@sap/
+        ],
+        resolve: {
+            extensions: ['.ts', '.js']
+        },
+        plugins: plugins,
+        module: {
+            rules: [{
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'ts-loader',
+                    options: {
+                        compilerOptions: {
+                            "module": "es6"
+                        }
+                    }
+                }]
             }]
-        }]
-    },
+        },
+    };
+
 };
 module.exports = config;
